e95fc659c493824582db6a3846cd849007ae26e4
Nicolás Chaves <nicash14@gmail.com>
2025-11-23 02:18:11 -0300

    Modificada variable de entorno para poder acceder con Vercel a la dirección en Render

---
+++ public/env.js
@@ -0,0 +1,3 @@
+window.env = {
+  apiUrl: 'http://localhost:3001'
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3001'  // URL del backend
+  apiUrl: (window as any)["env"]?.apiUrl || 'http://localhost:3001' // URL del backend
 };
